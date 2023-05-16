7ec592690fb2007596e0c9312c45c9bcb075306a
typefloat <liuzekun_123@163.com>
2023-05-16 09:57:05 +0800

    更新dockerfile与配置

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,7 @@ FROM python:3.8
 WORKDIR /app
 
 RUN pip install django && pip install openai
+
+ENV OPENAI_API_KEY=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
+
+CMD ["python3", "mygpt/manage.py", "runserver", "0.0.0.0:80"]
--- mygpt/mygpt/settings.py
+++ mygpt/mygpt/settings.py
@@ -15,9 +15,6 @@ SECRET_KEY = 'django-insecure-_2^5&=nqdm8s(dhu$*heka_5dv1nb&457s*)r&8o8*f27tith$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -93,13 +90,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
+ALLOWED_HOSTS = ["*"]
+LANGUAGE_CODE = "zh-hans"
+TIME_ZONE = "Asia/Shanghai"
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
